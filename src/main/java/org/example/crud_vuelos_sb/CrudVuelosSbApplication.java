7bbcc5bc36d630e984d34337245f01867a1f30c1
Davide Pinna <davidepinna94@gmail.com>
2025-12-18 19:45:47 +0000

    First commit

---
+++ src/main/java/org/example/crud_vuelos_sb/CrudVuelosSbApplication.java
@@ -0,0 +1,13 @@
+package org.example.crud_vuelos_sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudVuelosSbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudVuelosSbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/crud_vuelos_sb/CrudVuelosSbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.crud_vuelos_sb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudVuelosSbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
