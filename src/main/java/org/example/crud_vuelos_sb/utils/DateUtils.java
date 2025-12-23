ac7cda1b9c8db863d9d614e06ad6316dd8189087
Davide Pinna <davidepinna94@gmail.com>
2025-12-23 20:30:27 +0000

    Creacion DateUtils

---
+++ src/main/java/org/example/crud_vuelos_sb/utils/DateUtils.java
@@ -0,0 +1,13 @@
+package org.example.crud_vuelos_sb.utils;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+public class DateUtils {
+    // Válida que la fecha de salida sea anterior o igual a la de llegada
+    public static boolean esPeriodoValido(LocalDate salida, LocalDate llegada){
+        if (salida == null || llegada == null) return false;
+        return !salida.isAfter(llegada);
+    }
+}
